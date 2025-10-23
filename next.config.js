aa4993fcf044d1d442b9467aef6a6fd512d4fa8e
12core1 <12533753+visnkmr@users.noreply.github.com>
2025-10-24 02:43:56 +0530

    fix for auto build

---
+++ next.config.js
@@ -0,0 +1,30 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+// import type { NextConfig } from "next";
+// import withPWA from 'next-pwa'
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// async function setup() {
+//   if (process.env.NODE_ENV === 'development') {
+//     await setupDevPlatform()
+//   }
+// }
+// @ts-expect-error "ignore the error"
+// import pkg from 'next-pwa/cache.js'
+// const {runtimeCaching} = pkg
+
+// setup()
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  // basePath: '/sortedprocs',
+  // assetPrefix: '/sortedprocs',
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
